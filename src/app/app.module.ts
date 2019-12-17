1b9e18558924492768fc38edaf1072735b859482
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2019-12-17 16:31:32 +0100

    install primeng + import panelMenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {PanelMenuModule} from 'primeng/panelmenu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
 	imports: [
 		BrowserModule,
+		PanelMenuModule,
+		BrowserAnimationsModule
 	],
 	declarations: [
 		AppComponent,

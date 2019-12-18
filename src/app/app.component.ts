43ee9d6119cce6fc9ea21abacdd085dabe7af4db
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2019-12-18 02:30:03 +0100

    add navigation + create new directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ItemService } from './services/item.service';
 import { Item } from './models/item';
+import { EventTargetLike } from 'rxjs/internal/observable/fromEvent';
 // import {FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
@@ -12,7 +13,10 @@ import { Item } from './models/item';
 export class AppComponent implements OnInit {
 	menuItems: MenuItem[];
 	items: Item[];
-	selectedFile:any = null;
+	selectedFile: File;
+
+	parentId: string = "";
+	navigationPipe: string[] = [];
 	// uploadForm: FormGroup;  
 
 	constructor(
@@ -29,129 +33,143 @@ export class AppComponent implements OnInit {
 		// 	profile: ['']
 		// });
 
-		this.itemService.getItems()
+		this.loadItems();
+
+	}
+
+	loadItems() {
+		this.itemService.getItems(this.parentId)
 			.subscribe((items: Item[]) => {
 				console.log("items OK", items);
 				this.items = items;
 			});
-
 	}
 
-	onFileSelect(event:Event){
-		console.log("upload FILE***", event.target.files[0]);
-		this.selectedFile = event.target.files[0];
-		
-		// this.itemService.uploadFile(event.target.files[0])
-		// .subscribe(res=>{
-		// 	console.log('RESSSSS',res);
-		// });
-
-		// if (event.target.files.length > 0) {
-		// 	const file = event.target.files[0];
-		// 	this.uploadForm.get('profile').setValue(file);
-		//   }		
+	onNavigate(itemId: string) {
+		console.log("Navigate to Item", itemId);
+		this.navigationPipe.push(this.parentId);
+		this.parentId = itemId;
+		this.loadItems();
 	}
 
-	onUploadFile(){
+	onGoBack() {
+		console.log("Nav Back");
+		if (this.navigationPipe.length) {
+			let previousParentId = this.navigationPipe.pop();
+			this.parentId = (previousParentId ? previousParentId : "");
+			this.loadItems();
+		}
+	}
+
+	onFileSelect(event: any) {
+		if (event && event.target && event.target.files) {
+			console.log("upload FILE***", event.target.files[0]);
+			this.selectedFile = event.target.files[0];
+		}
+	}
+
+	onUploadFile() {
 		this.itemService.uploadFile(this.selectedFile)
-		.subscribe(res=>{
-			console.log('Uploaded 1212 12121 2121 21',res);
-		});
+			.subscribe(res => {
+				console.log('Uploaded 1212 12121 2121 21', res);
+			});
 	}
 
-	onDeleteItem(fileId:string){
+	onDeleteItem(fileId: string) {
 		this.itemService.deleteItem(fileId)
-		.subscribe(
-			() => console.log("Item deleted"),
-			(error:any) => console.log("Item Delete ERROR! "+ error)	
-		)
+			.subscribe(
+				() => console.log("Item deleted"),
+				(error: any) => console.log("Item Delete ERROR! " + error)
+			)
 	}
 
-	// download(url:string){
-	// 	console.log("download===", url);
-	// 	this.itemService.downloadItem(url)
-	// 	.subscribe(res => console.log, error => console.log);
+	onNewDirectory(folderName: string) {
+		console.log("new folder name", folderName);
+		this.itemService.newDirectory(this.parentId, folderName)
+			.subscribe(
+				() => console.log("Directory created"),
+				(error: any) => console.log("Directory creation ERROR! " + error)
+			)
+	}
+
+	// updateItems() {
+	// 	this.menuItems = [
+	// 		{ label: 'Open', icon: 'pi pi-fw pi-external-link' },
+	// 		{
+	// 			label: 'File',
+	// 			icon: 'pi pi-pw pi-file',
+	// 			items: [{
+	// 				label: 'New',
+	// 				icon: 'pi pi-fw pi-plus',
+	// 				items: [
+	// 					{ label: 'User', icon: 'pi pi-fw pi-user-plus' },
+	// 					{ label: 'Filter', icon: 'pi pi-fw pi-filter' }
+	// 				]
+	// 			},
+	// 			{ label: 'Open', icon: 'pi pi-fw pi-external-link' },
+	// 			{ separator: true },
+	// 			{ label: 'Quit', icon: 'pi pi-fw pi-times' }
+	// 			]
+	// 		},
+	// 		{
+	// 			label: 'Edit',
+	// 			icon: 'pi pi-fw pi-pencil',
+	// 			items: [
+	// 				{ label: 'Delete', icon: 'pi pi-fw pi-trash' },
+	// 				{ label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+	// 			]
+	// 		},
+	// 		{
+	// 			label: 'Help',
+	// 			icon: 'pi pi-fw pi-question',
+	// 			items: [
+	// 				{
+	// 					label: 'Contents',
+	// 					icon: 'pi pi-pi pi-bars'
+	// 				},
+	// 				{
+	// 					label: 'Search',
+	// 					icon: 'pi pi-pi pi-search',
+	// 					items: [
+	// 						{
+	// 							label: 'Text',
+	// 							items: [
+	// 								{
+	// 									label: 'Workspace'
+	// 								}
+	// 							]
+	// 						},
+	// 						{
+	// 							label: 'User',
+	// 							icon: 'pi pi-fw pi-file',
+	// 						}
+	// 					]
+	// 				}
+	// 			]
+	// 		},
+	// 		{
+	// 			label: 'Actions',
+	// 			icon: 'pi pi-fw pi-cog',
+	// 			items: [
+	// 				{
+	// 					label: 'Edit',
+	// 					icon: 'pi pi-fw pi-pencil',
+	// 					items: [
+	// 						{ label: 'Save', icon: 'pi pi-fw pi-save' },
+	// 						{ label: 'Update', icon: 'pi pi-fw pi-save' },
+	// 					]
+	// 				},
+	// 				{
+	// 					label: 'Other',
+	// 					icon: 'pi pi-fw pi-tags',
+	// 					items: [
+	// 						{ label: 'Delete', icon: 'pi pi-fw pi-minus' }
+	// 					]
+	// 				}
+	// 			]
+	// 		}
+	// 	];
 	// }
-
-	updateItems() {
-		this.menuItems = [
-			{ label: 'Open', icon: 'pi pi-fw pi-external-link' },
-			{
-				label: 'File',
-				icon: 'pi pi-pw pi-file',
-				items: [{
-					label: 'New',
-					icon: 'pi pi-fw pi-plus',
-					items: [
-						{ label: 'User', icon: 'pi pi-fw pi-user-plus' },
-						{ label: 'Filter', icon: 'pi pi-fw pi-filter' }
-					]
-				},
-				{ label: 'Open', icon: 'pi pi-fw pi-external-link' },
-				{ separator: true },
-				{ label: 'Quit', icon: 'pi pi-fw pi-times' }
-				]
-			},
-			{
-				label: 'Edit',
-				icon: 'pi pi-fw pi-pencil',
-				items: [
-					{ label: 'Delete', icon: 'pi pi-fw pi-trash' },
-					{ label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-				]
-			},
-			{
-				label: 'Help',
-				icon: 'pi pi-fw pi-question',
-				items: [
-					{
-						label: 'Contents',
-						icon: 'pi pi-pi pi-bars'
-					},
-					{
-						label: 'Search',
-						icon: 'pi pi-pi pi-search',
-						items: [
-							{
-								label: 'Text',
-								items: [
-									{
-										label: 'Workspace'
-									}
-								]
-							},
-							{
-								label: 'User',
-								icon: 'pi pi-fw pi-file',
-							}
-						]
-					}
-				]
-			},
-			{
-				label: 'Actions',
-				icon: 'pi pi-fw pi-cog',
-				items: [
-					{
-						label: 'Edit',
-						icon: 'pi pi-fw pi-pencil',
-						items: [
-							{ label: 'Save', icon: 'pi pi-fw pi-save' },
-							{ label: 'Update', icon: 'pi pi-fw pi-save' },
-						]
-					},
-					{
-						label: 'Other',
-						icon: 'pi pi-fw pi-tags',
-						items: [
-							{ label: 'Delete', icon: 'pi pi-fw pi-minus' }
-						]
-					}
-				]
-			}
-		];
-	}
-
 
 }
 
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -19,10 +19,12 @@ export class ItemService {
     console.log("api", this.apiEndPoint);
   }
 
-  getItems(url?: string) {
-    if (!url)
-      url = "/api/items";
-    return this.http.get(this.apiEndPoint + url)
+  getItems(parentId:string) {
+    let urlParameter = "";
+    if(parentId){
+      urlParameter += "?parentId="+parentId
+    }
+    return this.http.get(this.apiEndPoint+ "/api/items" + urlParameter)
       .pipe(
         map((response: any) => {
           return response.items.map((item: any) => {
@@ -42,6 +44,17 @@ export class ItemService {
 
   deleteItem(fileId: string) {
     return this.http.delete(this.apiEndPoint + "/api/items/" + fileId);
+  }
+
+  newDirectory(parentId:string,folderName:string){
+    let urlParameter = "";
+    if(parentId){
+      urlParameter += "?parentId="+parentId
+    }
+    return this.http.post(this.apiEndPoint + "/api/items"+urlParameter,{
+      "name": folderName,
+      "folder": true      
+    });
   }
 
   uploadFile(file: any) {
@@ -77,44 +90,44 @@ export class ItemService {
     // );
   }
 
-  private showProgress(message: any) {
-    console.log("showProgress==== ", message);
+  // private showProgress(message: any) {
+  //   console.log("showProgress==== ", message);
 
-  }
+  // }
 
-  private getEventMessage(event: HttpEvent<any>, file: File) {
-    switch (event.type) {
-      case HttpEventType.Sent:
-        return `Uploading file "${file.name}" of size ${file.size}.`;
+  // private getEventMessage(event: HttpEvent<any>, file: File) {
+  //   switch (event.type) {
+  //     case HttpEventType.Sent:
+  //       return `Uploading file "${file.name}" of size ${file.size}.`;
 
-      case HttpEventType.UploadProgress:
-        // Compute and show the % done:
-        const percentDone = Math.round(100 * event.loaded / event.total);
-        return `File "${file.name}" is ${percentDone}% uploaded.`;
+  //     case HttpEventType.UploadProgress:
+  //       // Compute and show the % done:
+  //       const percentDone = Math.round(100 * event.loaded / event.total);
+  //       return `File "${file.name}" is ${percentDone}% uploaded.`;
 
-      case HttpEventType.Response:
-        return `File "${file.name}" was completely uploaded!`;
+  //     case HttpEventType.Response:
+  //       return `File "${file.name}" was completely uploaded!`;
 
-      default:
-        return `File "${file.name}" surprising upload event: ${event.type}.`;
-    }
-  }
+  //     default:
+  //       return `File "${file.name}" surprising upload event: ${event.type}.`;
+  //   }
+  // }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
+  // private handleError(error: HttpErrorResponse) {
+  //   if (error.error instanceof ErrorEvent) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('An error occurred:', error.error.message);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong,
+  //     console.error(
+  //       `Backend returned code ${error.status}, ` +
+  //       `body was: ${error.error}`);
+  //   }
+  //   // return an observable with a user-facing error message
+  //   return throwError(
+  //     'Something bad happened; please try again later.');
+  // };
 
   // uploadFile(file){
   //   this.http.post(this.apiEndPoint+"/api/items/",file);
